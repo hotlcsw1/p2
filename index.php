d3d4011198cc1765fab3802cbd085f52d80ca344
Arcot Prakash <arcotprakash@g.harvard.edu>
2015-09-23 13:07:50 -0400

    Updated index.php-removed div tag.

--- index.php
+++ index.php
@@ -16,11 +16,9 @@
   xkcd Style Password Generator
 </h1>
 
-<div>
-  <span>
+<span>
     <h2><?php echo $displayPwd; ?></h2>
-  </span>
-</div>
+</span>
 
 <h5>
   What is this?: xkcd as per Wikipedia is a webcomic created by Randall Munroe.  He describes it as a "Webcomic of romance, sarcasm, math, and language".  I describe it as clever and intellectually entertaining way to learn and understand models.  Using a model for password creation as suggested by him (see below), I have used php, html and some JavaScript to generate random passwords.  I used 30 words, 10 digits (0-9) and special characters such as $, %, ^, etc. to create this password.  You can try playing with it and see what you think.  You can select between 1-9 range for # of words and whether you would like numbers and special characters when the password is generated. I hope you have fun with it.
