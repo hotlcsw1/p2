d48a51c55ec3e80328f64f4c3b0ade6ed48b217d
Arcot Prakash <arcotprakash@g.harvard.edu>
2015-09-22 20:08:51 -0400

    Adding README, defaultstyle.css, index.php and logic.php files.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Project 2 - Dynamic Web Applications
+
+Author       : Arcot Prakash
+Date Created : 9/22/2015
+Date Updated : 9/22/2015
+Time Updated : 8:06 PM
+
+Live URL:
+p2.approjects.me
+
+Description:
+This is a project to generate word passwords that can include numbers and special characters.  In this lesson I learned how to use PHP's random functions, arrays, looping, etc.  I also learned a few more css commands to  center an image, text, etc.  The project is based of xkcd style password.
+
+Demo:
+Not yet DONE!
+
+Details for the teaching team:
+No login required
+
+Plugins, libraries, package or outside code:
+None
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Arcot Prakash - Project 2 - xkcd Style Password Generator</title>
+  <link rel="stylesheet" type="text/css" href="defaultstyle.css">
+  <meta charset = "utf-8">
+  <meta name = "author" content = "Arcot Prakash">
+  <meta name = "description" content ="xkcd Style Password">
+</head>
+
+<h1>
+  Arcot Prakash
+  <br>
+  xkcd Style Password Generator
+</h1>
+
+<form method="post" action="index.php">
+    <P class="blocktext">
+  # of Words <input type="text" name = "numberOfWords"> (Max 9)<br>
+  <input type="checkbox" name="addNumber" value=""> Add a number<br>
+  <input type="checkbox" name="addSymbol"> Add a symbol<br>
+  <br>
+  <button type="submit">Gimme Another</button>
+  <br>
+</form>
+
+<a href ="http://xkcd.com/936/">
+  <img src="http://imgs.xkcd.com/comics/password_strength.png" alt="Link to xkcd" class="center" style="width:520px;height:420px;border:0">
+</a>
+
+</body>
+</html>
